cf46a23bd09f333ecffa63bca9324d6bf2627e57
Leandro <leandro.a.estrada@gmail.com>
2021-08-30 21:27:39 -0300

    Adding Models

---
+++ EventsAppIOS/EventsAppIOS/Models/Participant.swift
@@ -0,0 +1,12 @@
+//
+//  Participant.swift
+//  EventsAppIOS
+//
+//  Created by leandromac on 30/08/21.
+//
+
+import Foundation
+
+struct Participant: Decodable {
+    
+}
---
+++ EventsAppIOS/EventsAppIOS/Models/Ticket.swift
@@ -0,0 +1,12 @@
+//
+//  Ticket.swift
+//  EventsAppIOS
+//
+//  Created by leandromac on 30/08/21.
+//
+
+import Foundation
+
+struct Ticket: Decodable {
+    
+}
